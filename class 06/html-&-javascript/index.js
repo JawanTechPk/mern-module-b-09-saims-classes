59c06ac2d765adf74c3f72998db292fe0d03677e
annasshah <30356728+annasshah@users.noreply.github.com>
2023-12-24 00:07:21 +0500

    class 06

---
+++ class 06/html-&-javascript/index.js
@@ -0,0 +1,117 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const filter_arr = () => {
+//   let filter_data = [];
+//   for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+
+//     const check_cond = element % 2 === 0;
+//     if (check_cond) {
+//       filter_data.push(element);
+//     }
+//   }
+//   return filter_data;
+// };
+
+// const filter_by_filter_method = arr.filter((element) => element % 2 === 0);
+
+
+// let arr = [
+//   {
+//       brand: "Samsung",
+//       model: "SamsungA30",
+//       price: "40000",
+//       camera: "40px",
+//       ram: "8gb",
+//       rom : "128gb",
+      
+//   },
+//   {
+//       brand: "Samsung",
+//       model: "SamsungA10",
+//       price: "40000",
+//       camera: "40px",
+//       ram: "4gb",
+//       rom : "128gb",
+      
+//   },
+//   {
+//       brand: "Samsung",
+//       model: "SamsungA20",
+//       price: "40000",
+//       camera: "40px",
+//       ram: "4gb",
+//       rom : "128gb",
+      
+//   },
+//   {
+//       brand: "Vivo",
+//       model: "VivoY20",
+//       price: "20000",
+//       camera: "40px",
+//       ram: "2gb",
+//       rom : "32gb",
+      
+//   },
+//   {
+//       brand: "Vivo",
+//       model: "VivoY11",
+//       price: "20000",
+//       camera: "40px",
+//       ram: "3gb",
+//       rom : "32gb",
+      
+//   },
+//   {
+//       brand: "Vivo",
+//       model: "VivoY15",
+//       price: "30000",
+//       camera: "40px",
+//       ram: "4gb",
+//       rom : "64gb",
+      
+//   },
+//   {
+//       brand: "Motorola",
+//       model: "Motorola123",
+//       price: "5000",
+//       camera: "2px",
+//       ram: "2gb",
+//       rom : "32gb",
+      
+//   },
+//   {
+//       brand: "Iphone",
+//       model: "Iphone12",
+//       price: "300000",
+//       camera: "40px",
+//       ram: "8gb",
+//       rom : "128gb",
+      
+//   },
+//   {
+//       brand: "Iphone",
+//       model: "Iphone13",
+//       price: "300000",
+//       camera: "40px",
+//       ram: "8gb",
+//       rom : "128gb",
+      
+//   },
+//   {
+//       brand: "Iphone",
+//       model: "IphoneX",
+//       price: "300000",
+//       camera: "40px",
+//       ram: "8gb",
+//       rom : "128gb",
+      
+//   },
+// ]
+
+
+//  render an object keys
+//  apply filter against the selected key
+// display the searched result
+
+
