a93a32b74b44ce8ecdeda31e32c0ad7105d39c07
annasshah <30356728+annasshah@users.noreply.github.com>
2023-12-24 00:08:32 +0500

    --

---
+++ class 06/react js/src/App.js
@@ -0,0 +1,18 @@
+import { MyComponent, MyComponent_02 } from "./MyComponent";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+
+      <MyComponent />
+      <MyComponent_02 />
+    </>
+  );
+}
+
+export default App;
---
+++ class 06/react js/src/MyComponent.js
@@ -0,0 +1,17 @@
+const MyComponent = () => {
+  return (
+    <>
+      <h1>Component 01</h1>
+    </>
+  );
+};
+
+const MyComponent_02 = () => {
+  return (
+    <>
+      <h1>Component 02</h1>
+    </>
+  );
+};
+
+export { MyComponent, MyComponent_02 };
